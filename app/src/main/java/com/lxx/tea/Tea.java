c290a542d4f0a6874624a2428d2a1a02d918e61a
luoyingxing <luoyingxing@126.com>
2019-03-04 19:32:37 +0800

    update

---
+++ app/src/main/java/com/lxx/tea/Tea.java
@@ -0,0 +1,28 @@
+package com.lxx.tea;
+
+import java.util.List;
+
+/**
+ * author:  luoyingxing
+ * date: 2019/3/4.
+ */
+public class Tea {
+    private String name;
+    private List<Integer> list;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public void setList(List<Integer> list) {
+        this.list = list;
+    }
+}
--- app/src/main/java/com/lxx/tea/TeaListActivity.java
+++ app/src/main/java/com/lxx/tea/TeaListActivity.java
@@ -1,20 +1,117 @@
 package com.lxx.tea;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.lyx.frame.adapter.abs.CommonAdapter;
+import com.lyx.frame.adapter.abs.ViewHolder;
+
+import net.tsz.afinal.FinalDb;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * TeaListActivity
  * <p/>
  * Created by luoyingxing on 2019/3/4.
  */
-public class TeaListActivity  extends AppCompatActivity {
+public class TeaListActivity extends AppCompatActivity {
+    private ListView listView;
+
+    private CommonAdapter<Tea> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        listView = findViewById(R.id.lv_list);
+
+        adapter = new CommonAdapter<Tea>(this, new ArrayList<Tea>(), R.layout.item_list) {
+            @Override
+            protected void convert(ViewHolder holder, final Tea item, int position) {
+                holder.setText(R.id.tv_list_index, position + "、");
+                holder.setText(R.id.tv_list_name, item.getName());
+
+                holder.getView(R.id.iv_edit).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+                holder.getView(R.id.iv_delete).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FinalDb mFinalDb = FinalDb.create(getApplicationContext());
+                        mFinalDb.delete(item);
+                        adapter.remove(item);
+                    }
+                });
+            }
+        };
+        listView.setAdapter(adapter);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        load();
+    }
+
+    private void load() {
+        FinalDb mFinalDb = FinalDb.create(getApplicationContext());
+        List<Tea> list = mFinalDb.findAll(Tea.class);
+        Collections.reverse(list); // 倒序排列
+
+        adapter.clear();
+        adapter.addAll(list);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void save(Tea tea) {
+        FinalDb mFinalDb = FinalDb.create(getApplicationContext());
+        mFinalDb.save(tea);
+    }
+
+    private void add() {
+        final EditText editText = new EditText(this);
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("添加茶品")
+                .setView(editText)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = editText.getText().toString().trim();
+
+                        if (TextUtils.isEmpty(name)) {
+                            Toast.makeText(getApplicationContext(), "茶品名称不能为空！", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Tea tea = new Tea();
+                            tea.setName(name);
+                            save(tea);
+                            dialog.dismiss();
+                            load();
+                        }
+                    }
+                });
+    }
+
+
 }
