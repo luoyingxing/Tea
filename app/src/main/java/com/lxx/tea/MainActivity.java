0f8e8be5d365c17b0289dc33ed08878c725266e5
luoyingxing <luoyingxing@126.com>
2019-03-05 09:58:39 +0800

    add BaseAdapter

--- app/src/main/java/com/lxx/tea/MainActivity.java
+++ app/src/main/java/com/lxx/tea/MainActivity.java
@@ -1,15 +1,26 @@
 package com.lxx.tea;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import net.tsz.afinal.FinalDb;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner mSpinner;
@@ -31,6 +42,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mStartTV.setOnClickListener(this);
         mResetTV.setOnClickListener(this);
+
+        adapter = new SpAdapter(this, new ArrayList<Tea>());
+        mSpinner.setAdapter(adapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mTea = adapter.getItem(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private SpAdapter adapter;
+    private Tea mTea;
+
+    private class SpAdapter extends BaseAdapter {
+        private List<Tea> list;
+        private LayoutInflater mInflater;
+
+        public SpAdapter(Context context, List<Tea> l) {
+            list = l;
+            mInflater = LayoutInflater.from(context);
+        }
+
+        public void clear() {
+            list.clear();
+            notifyDataSetChanged();
+        }
+
+        public void addAll(List<Tea> l) {
+            list.addAll(l);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Tea getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return list.get(position).getId();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //将布局文件转化为View对象
+            @SuppressLint("ViewHolder")
+            View view = mInflater.inflate(R.layout.item_spinner, null);
+
+            TextView textView = view.findViewById(R.id.tv_main_item);
+
+            Tea tea = list.get(position);
+
+            textView.setText(tea.getName());
+
+            return view;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getData();
+    }
+
+    public void getData() {
+        FinalDb mFinalDb = FinalDb.create(getApplicationContext());
+        List<Tea> list = mFinalDb.findAll(Tea.class);
+
+        adapter.clear();
+        adapter.addAll(list);
     }
 
     @Override
